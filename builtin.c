40c3e7716206c8e20faf89aa6090250de5744d3f
yousefelbadawy <114675087+Yousefelbadawy@users.noreply.github.com>
2023-07-29 00:00:17 +0300

    Update builtin.c

--- builtin.c
+++ builtin.c
@@ -1,11 +1,18 @@
+
 #include "shell.h"
 
-
+/**
+ * _myexit - exits the shell
+ * @info: Structure containing potential arguments. Used to maintain
+ * constant function prototype.
+ * Return: exits with a given exit status
+ * (0) if info.argv[0] != "exit"
+ */
 int _myexit(info_t *info)
 {
 	int exitcheck;
 
-	if (info->argv[1]) 
+	if (info->argv[1]) /* If there is an exit arguement */
 	{
 		exitcheck = _erratoi(info->argv[1]);
 		if (exitcheck == -1)
@@ -23,7 +30,12 @@ int _myexit(info_t *info)
 	return (-2);
 }
 
-
+/**
+ * _mycd - changes the current directory of the process
+ * @info: Structure containing potential arguments. Used to maintain
+ * constant function prototype.
+ * Return: Always 0
+ */
 int _mycd(info_t *info)
 {
 	char *s, *dir, buffer[1024];
@@ -42,19 +54,8 @@ int _mycd(info_t *info)
 			chdir_ret = chdir(dir);
 	}
 	else if (_strcmp(info->argv[1], "-") == 0)
-	
-  {
-	if (chdir_ret == -1)
 	{
-		print_error(info, "can't cd to ");
-		_eputs(info->argv[1]), _eputchar('\n');
-	}
-	else
-	{
-		_setenv(info, "OLDPWD", _getenv(info, "PWD="));
-		_setenv(info, "PWD", getcwd(buffer, 1024));
-	}
-    if (!_getenv(info, "OLDPWD="))
+		if (!_getenv(info, "OLDPWD="))
 		{
 			_puts(s);
 			_putchar('\n');
@@ -66,7 +67,16 @@ int _mycd(info_t *info)
 	}
 	else
 		chdir_ret = chdir(info->argv[1]);
-	
+	if (chdir_ret == -1)
+	{
+		print_error(info, "can't cd to ");
+		_eputs(info->argv[1]), _eputchar('\n');
+	}
+	else
+	{
+		_setenv(info, "OLDPWD", _getenv(info, "PWD="));
+		_setenv(info, "PWD", getcwd(buffer, 1024));
+	}
 	return (0);
 }
 
@@ -78,6 +88,6 @@ int _myhelp(info_t *info)
 	arg_array = info->argv;
 	_puts("help call works. Function not yet implemented \n");
 	if (0)
-		_puts(*arg_array); 
+		_puts(*arg_array); /* temp att_unused workaround */
 	return (0);
 }
